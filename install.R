b03d7f67d8d4cc95522425ce7424558770b36fb6
gkenyon <gkenyon@gmail.com>
2019-09-30 17:03:57 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,2 +1,2 @@
-install.packages(c("devtools", "rstudioapi", "tidyverse","rvest","httr","readxl","janitor","glue","Hmisc"), dependencies=TRUE, repos=c("http://rstudio.org/_packages", "http://cran.rstudio.com"))
+install.packages(c("dplyr", "devtools", "rstudioapi", "tidyverse","rvest","httr","readxl","janitor","glue","Hmisc"), dependencies=TRUE, repos=c("http://rstudio.org/_packages", "http://cran.rstudio.com"))
 devtools::install_github(repo = "FantasyFootballAnalytics/ffanalytics", build_vignettes = TRUE)
